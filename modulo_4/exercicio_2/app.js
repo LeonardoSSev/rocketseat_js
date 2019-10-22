79c2dfeead71a3a647f404129a1ea8fa98229cb3
Leonardo Santos Severino <leonardossev@gmail.com>
2019-10-22 00:36:52 -0300

    Adiciona script do exercício 2 do módulo 4

---
+++ modulo_4/exercicio_2/app.js
@@ -0,0 +1,48 @@
+function adicionar () {
+  const nome = document.querySelector('input[name=usuario]').value.trim();
+  if (!nome || nome === '') {
+    return;
+  }
+
+  const url = `https://api.github.com/users/${nome}`;
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.send();
+
+  let repositorioURL = '';
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const response = JSON.parse(xhr.response);
+      
+      repositorioURL = response.repos_url
+      buscaRepositorios(repositorioURL);
+    }
+  }
+}
+
+function buscaRepositorios(url) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.send();
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const repositorios = JSON.parse(xhr.response);
+    
+      repositorios.forEach(repositorio => {
+        adicionaItem(repositorio.name);
+      })
+    }
+  }
+}
+
+function adicionaItem(nome) {
+  const lista = document.querySelector('#lista');
+
+  const linha = document.createElement('li');
+  const conteudoDaLinha = document.createTextNode(nome);
+
+  linha.appendChild(conteudoDaLinha);
+  lista.appendChild(linha);
+}
